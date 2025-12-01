b065cf4bbf910769a646a6fda1d94c26d2c7da2c
二叉树树 <acofork@qq.com>
2025-12-01 21:05:40 +0800

    docs(todo): 更新待办事项列表状态和内容
    - 将"利用STUN在世界各地连接上你的电脑"和"anuneko.com米哈游AI上手体验"标记为已完成
    - 添加新任务"上手ESA Pages"
    - 保持其他任务状态不变

--- src/data/todo.ts
+++ src/data/todo.ts
@@ -5,10 +5,11 @@ export const todoConfig: TodoConfig = {
     title: "待办事项",
     items: [
         { content: "为博客添加”待办事项“功能", completed: true },
-        { content: "做视频：利用STUN在世界各地连接上你的电脑", completed: false  },
+        { content: "做视频：利用STUN在世界各地连接上你的电脑", completed: true  },
         { content: "做视频：Ventoy+FirPE使用（可能不会做）", completed: false },
 		{ content: "做视频：Cloudflare 利用Origin Rules 6转4 访问家里云", completed: false },
-        { content: "做视频：anuneko.com米哈游AI上手体验", completed: false },
-        { content: "做Bot插件：anuneko.com米哈游AI聊天机器人", completed:true },
+        { content: "做视频或文章：上手ESA Pages", completed: false },
+        { content: "做视频：anuneko.com米哈游AI上手体验", completed: true },
+        { content: "做Bot插件：anuneko.com米哈游AI聊天机器人", completed: true },
     ],
 };
