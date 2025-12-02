9b8f1143d66be15e1478507653d551649d5ba6a5
二叉树树 <acofork@qq.com>
2025-12-02 13:57:28 +0800

    refactor(todo): 移除已完成或不再计划的ESA Pages任务项

--- src/data/todo.ts
+++ src/data/todo.ts
@@ -8,7 +8,6 @@ export const todoConfig: TodoConfig = {
         { content: "做视频：利用STUN在世界各地连接上你的电脑", completed: true  },
         { content: "做视频：Ventoy+FirPE使用（可能不会做）", completed: false },
 		{ content: "做视频：Cloudflare 利用Origin Rules 6转4 访问家里云", completed: false },
-        { content: "做视频或文章：上手ESA Pages", completed: false },
         { content: "做视频：anuneko.com米哈游AI上手体验", completed: true },
         { content: "完善Bot插件：anuneko.com米哈游AI聊天机器人（支持pick）", completed: true },
 { content: "写文章：当anuneko bot插件完善后，编写开发文章", completed: true },
